98a33e3de4abd9879c6e6ff2746c4ba35e5fe4cd
Nikko <nikkodc12@gmail.com>
2019-10-19 20:14:59 +0800

    initial commit

---
+++ jquery/assets/js/script.js
@@ -0,0 +1,37 @@
+$(function(){
+
+	// Prevent any jQuery code from running before the document is finished loading (is ready).
+	$(document).ready(function() {
+		// jQuery methods go here...
+		
+		// Example 1
+		// var demo1 = $('.demo1').text();
+		// var demo2 = $('.demo2').text();
+		// var demo3 = $('.demo3').html();
+		// console.log(demo3);
+
+
+
+		// Example 2 
+		// $('.demo4').val('lorem');
+		// $('.demo5').text('lorem');
+		// $('.demo6').html("<h1>test</h1>");
+
+
+
+
+
+	});
+
+
+	$(document).scroll(function() {
+		/* Act on the event */
+		// alert()
+	});
+
+
+
+
+
+
+});
